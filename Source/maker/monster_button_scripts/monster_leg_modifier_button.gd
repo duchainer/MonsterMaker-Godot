181578f02b3c988085bc703a1fd27bd8f5250912
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-07-26 06:20:22 -0700

    feat: Add Legs tab.

---
+++ Source/maker/monster_button_scripts/monster_leg_modifier_button.gd
@@ -0,0 +1,5 @@
+class_name MonsterLegModifierButton
+extends MonsterModifierButton
+
+func apply():
+	controller.set_leg(texture_normal)
--- Source/maker/monster_controller.gd
+++ Source/maker/monster_controller.gd
@@ -4,7 +4,8 @@ extends Node2D
 @onready var body = %Body
 @onready var left_arm_sprite = %LeftArmSprite
 @onready var right_arm_sprite = %RightArmSprite
-
+@onready var right_leg_sprite = %RightLegSprite
+@onready var left_leg_sprite = %LeftLegSprite
 
 func set_body(texture : Texture2D):
 	body.texture = texture
@@ -12,3 +13,7 @@ func set_body(texture : Texture2D):
 func set_arm(texture : Texture2D):
 	left_arm_sprite.texture = texture
 	right_arm_sprite.texture = texture
+
+func set_leg(texture : Texture2D):
+	right_leg_sprite.texture = texture
+	left_leg_sprite.texture = texture
